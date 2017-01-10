86aa743d739a3a0a9a69c16908d59f6e7d383263
ss75710541 <root@package1.10-3-10-33.crawler.ucloud-bj-bgp-c.dm>
2017-01-10 17:59:41 +0800

    add docker filebeat

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build $opts -t library/centos7-filebeat-5.0.2 -f Dockerfile_runtime .
---
+++ run.sh
@@ -0,0 +1,6 @@
+docker run -d \
+        -e LOGSTASH_HOST=10.157.21.2 \
+        -e LOGSTASH_PORT=5043 \
+	-v /data/filebeat:/usr/share/filebeat/bin/data \
+	-v /data/opt/app/logs:/opt/app/logs \
+        library/centos7-filebeat-5.0.2
