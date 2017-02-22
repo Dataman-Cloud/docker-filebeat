7544d8a66198fb4e53066ae4746fbe5015294372
ss75710541 <root@package1.10-3-10-33.crawler.ucloud-bj-bgp-c.dm>
2017-02-22 18:46:18 +0800

    update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ ETH=${ETH:-eth0}
 SYSTEMIP=`ip addr show $ETH|grep inet.*brd|awk '{print $2}'|awk -F/ '{print $1}'`
 
 # Render config file
-cat filebeat.yml | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" | sed "s|INDEX|${INDEX}|g" | sed "s|DOCUMENT_TYPE|${DOCUMENT_TYPE}|g" | sed "s|PATHS|${PATHS}|g" | sed "s|SYSTEMIP|${SYSTEMIP}" > filebeat.yml.tmp
+cat filebeat.yml | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" | sed "s|INDEX|${INDEX}|g" | sed "s|DOCUMENT_TYPE|${DOCUMENT_TYPE}|g" | sed "s|PATHS|${PATHS}|g" | sed "s|SYSTEMIP|${SYSTEMIP}|g" > filebeat.yml.tmp
 cat filebeat.yml.tmp > filebeat.yml
 rm filebeat.yml.tmp
 
