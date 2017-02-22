25e92941767ec9f6d0d65c2d9ab80a5b7945af4d
ss75710541 <root@package1.10-3-10-33.crawler.ucloud-bj-bgp-c.dm>
2017-02-22 16:29:16 +0800

    update filebeat.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
+PATHS=${PATHS:-/opt/app/logs/*/*.log}
+
+DOCUMENT_TYPE=${DOCUMENT_TYPE:-log}
+
 # Render config file
-cat filebeat.yml | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" | sed "s|INDEX|${INDEX}|g" | sed "s|DOCUMENT_TYPE|${DOCUMENT_TYPE}|g" > filebeat.yml.tmp
+cat filebeat.yml | sed "s|LOGSTASH_HOST|${LOGSTASH_HOST}|g" | sed "s|LOGSTASH_PORT|${LOGSTASH_PORT}|g" | sed "s|INDEX|${INDEX}|g" | sed "s|DOCUMENT_TYPE|${DOCUMENT_TYPE}|g" | sed "s|PATHS|${PATHS}|g" > filebeat.yml.tmp
 cat filebeat.yml.tmp > filebeat.yml
 rm filebeat.yml.tmp
 
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ docker run -d \
         -e LOGSTASH_HOST=10.157.21.2 \
         -e LOGSTASH_PORT=5043 \
 	-e DOCUMENT_TYPE=tengine_access \
+        -e PATHS="/docker/tengine/logs/*-access.log" \
 	-v /data/filebeat:/usr/share/filebeat/bin/data \
 	-v /data/opt/app/logs:/opt/app/logs \
         library/centos7-filebeat-5.0.2
